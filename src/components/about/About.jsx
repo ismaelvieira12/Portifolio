06dc0ce54d1b298257edc8af179360984a85618b
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-12-13 15:24:48 -0300

    Update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 import "../about/about.css";
-
+import imagem from "../../assets/about.png"
 const About = () => {
   return (
-    <div>Testando pasta 8</div>
+    <section className="about container section">
+      <h2 className="section-titel">Sobre mim</h2>
+
+      <div className="about_container grid">
+          <img src={imagem} alt="testando"  className='about_img'/>
+
+          <div className="about_data grid">
+            <div className="about_data_infor">
+              <p className="about_description">Sou dedicado e disciplinado com uma paixão pela programação. Tenho experiência básica em HTML5, CSS3, JAVASCRIPT e MySQL. Estou constantemente em busca de inovações e me sinto motivado em resolver problemas complexos através da tecnologia.
+              Tenho capacidade de aprender rapidamente novos conceitos e tecnologias. Trabalhar em equipe é algo que valorizo, pois acredito que a colaboração é essencial para o sucesso de qualquer projeto. Sou detalhista e organizado.</p>
+              <a href="#" className='btn'>Download CV</a>
+
+
+              <div className="about_skills grid">
+                <div className="skills_data">
+                  <div className="skills_title">
+                    <h3 className="skills_names">Desenvolvedor</h3>
+                    <span className="skills_number">90%</span>
+                  </div>
+
+                  <div className="skills_bar">
+                    <span className="skills_percetage development"></span>
+                  </div>
+                </div>
+
+                <div className="skills_data">
+                  <div className="skills_title">
+                    <h3 className="skills_names">Design</h3>
+                    <span className="skills_number">70%</span>
+                  </div>
+
+                  <div className="skills_bar">
+                    <span className="skills_percetage ui_design"></span>
+                  </div>
+                </div>
+
+                <div className="skills_data">
+                  <div className="skills_title">
+                    <h3 className="skills_names">Photography</h3>
+                    <span className="skills_number">60%</span>
+                  </div>
+
+                  <div className="skills_bar">
+                    <span className="skills_percetage protography"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
   )
 }
 
