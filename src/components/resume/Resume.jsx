77151eb4c9f1f074d6bb59d0eb3b7c9af57a4f3f
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-12-25 22:36:40 -0300

    Update

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,7 +4,7 @@ import "../resume/resume.css";
 const Resume = () => {
   return (
     <div className="container section resume" id="resume">
-      
+      <h2 className="section_title">Experiência</h2>
     </div>
   )
 }
