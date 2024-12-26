2675d1d36dbf957fb25c8ab03f2242e022357f1b
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-12-25 23:39:24 -0300

    Finalizando a parte do Resume

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -3,7 +3,7 @@ const Data = [
         id: 1,
         category: "educação",
         icon: "icon-graduation",
-        year: 2022-2022,
+        year: '2022-2022',
         title: "HTML5 e CSS3",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
  
@@ -12,7 +12,7 @@ const Data = [
         id: 2,
         category: "experiência",
         icon: "icon-graduation",
-        year: 2022-2023,
+        year: '2022-2023',
         title: "JavaScript - Basico",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
  
@@ -21,8 +21,17 @@ const Data = [
         id: 3,
         category: "educação",
         icon: "icon-graduation",
-        year: 2023-2024,
+        year: '2023-2024',
         title: "JavaScript - Intermediário",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
+ 
+    },
+    {
+        id: 4,
+        category: "experiência",
+        icon: "icon-graduation",
+        year: '2024-2024',
+        title: "JavaScript - Avançado",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
  
     }
