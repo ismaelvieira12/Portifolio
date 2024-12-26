6aa4499873278f0e28d3543a0bf93e19394cf60b
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-12-25 22:46:44 -0300

    Update

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,32 @@
+const Data = [
+    {
+        charge_id: null,
+        charge_type: null,
+        company_id: 5049,
+        contract_id: 1096806,
+        created_at: "2024-06-03T14:51:51.000000Z",
+        custom_id: "725123361",
+        customer: {
+            id: 1089437,
+            name: "Elaine Steffane Rodrigues"
+            phone:{
+                
+            } 
+            id
+            : 
+            1022254
+            number
+            : 
+            "(85) 9 9701-6256"
+            number_only
+            : 
+            "85997016256"
+            phone_id
+            : 
+            1089437
+        }
+ 
+    }
+]
+
+export default Data
