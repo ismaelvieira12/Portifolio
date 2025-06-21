c8d1ef92a554401a8eb11ba9662e66e3ac189896
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-06-21 12:58:25 -0300

    Criando um carossel

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import "../portifolio/portifolio.css"
-// import Menu from './Menu';
+import Menu from './Menu';
 
 const Portifolio = () => {
   return (
-    <div className="container work section" id="work">
+    <section className="container work section" id="work">
       <h2 className="container-title">Projetos Recentes</h2>
 
       <div className="work_filters">
-        <div className="work_itens">Relógio</div>
-        <div className="work_itens">Jogo da Velha</div>
-        <div className="work_itens">Gerador de Senha</div>
-        <div className="work_itens">Dashboard</div>
-        <div className="work_itens">Projeto com bootstrap</div>
+        <span className="work_itens">Relógio</span>
+        <span className="work_itens">Jogo da Velha</span>
+        <span className="work_itens">Gerador de Senha</span>
+        <span className="work_itens">Dashboard</span>
+        <span className="work_itens">Projeto com bootstrap</span>
       </div>
 
       <div className="container grid">
-        {/* <Menu/> */}
+        <Menu/>
       </div>
-    </div>
+    </section>
   )
 }
 
