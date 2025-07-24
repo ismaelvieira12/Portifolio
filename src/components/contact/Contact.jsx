b74bab74508d0176ba4bcd9c31f6c75ce3ca993c
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-07-23 22:33:07 -0300

    Update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,23 +3,23 @@ import "../contact/contact.css";
 
 const Contact = () => {
   return (
-    <>
+    
     <section id='contact'className="container contact section">
-        <h2 className="section_title">Contato</h2>
+        <h2 className="section_title">Entre contato</h2>
       <form className='contact_form'>
-        <div>
+        <div className='form_group'>
           <input placeholder='Nome' type="text" name='name' className='nome'/>
         </div>
-        <div>
+        <div className='form_group'>
           <input placeholder='Email' type="email" name="email" className='email' />
         </div>
-        <div>
+        <div className='form_group_textarea'>
           <textarea placeholder='Escreva aqui...' name="Texto" id="textArea" className='textArea'></textarea>
         </div>
       </form>
 
     </section>
-    </>
+    
   );
 }
 
