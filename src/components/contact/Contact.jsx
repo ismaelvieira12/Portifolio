613aa8d5005c91c4200374ffd8880fc42cc08979
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-07-24 01:18:15 -0300

    Criando a parte de formulario

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,8 +5,17 @@ const Contact = () => {
   return (
     <>
     <section id='contact'className="container contact section">
-      <form>
         <h2 className="section_title">Contato</h2>
+      <form className='contact_form'>
+        <div>
+          <input placeholder='Nome' type="text" name='name' className='nome'/>
+        </div>
+        <div>
+          <input placeholder='Email' type="email" name="email" className='email' />
+        </div>
+        <div>
+          <textarea placeholder='Escreva aqui...' name="Texto" id="textArea" className='textArea'></textarea>
+        </div>
       </form>
 
     </section>
