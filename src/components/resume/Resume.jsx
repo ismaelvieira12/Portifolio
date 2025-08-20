e8ddb7ebc6da7b45afa329913717ef3d83b045be
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-08-20 16:53:21 -0300

    uPDATE

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,46 +1,27 @@
-import React from "react";
+import React from 'react';
 import "../resume/resume.css";
-import Data from "./Data";
-import Card from "./Card";
+import Data from './Data';
+import Card from './Card';
 
 const Resume = () => {
-  // Separando categorias para evitar repetição
-  const educacao = Data.filter((item) => item.category === "educação");
-  const experiencia = Data.filter((item) => item.category === "experiência");
-
   return (
     <section className="container section resume" id="resume">
-      <h2 className="section_title">Experiência</h2>
-
+      <h2 className="section_title">Formação Acadêmica</h2>
       <div className="resume_container grid">
         <div className="timeline grid">
-          <h3 className="timeline_header">Formação Acadêmica</h3>
-          {educacao.map((item) => (
+          {Data.map((val, id) => (
             <Card
-              key={item.id}
-              icon={item.icon}
-              title={item.title}
-              year={item.year}
-              desc={item.desc}
-            />
-          ))}
-        </div>
-
-        <div className="timeline grid">
-          <h3 className="timeline_header">Experiência Profissional</h3>
-          {experiencia.map((item) => (
-            <Card
-              key={item.id}
-              icon={item.icon}
-              title={item.title}
-              year={item.year}
-              desc={item.desc}
+              key={id}
+              icon={val.icon}
+              title={val.title}
+              year={val.year}
+              desc={val.desc}
             />
           ))}
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
 export default Resume;
