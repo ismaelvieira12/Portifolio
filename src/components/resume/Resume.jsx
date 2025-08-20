2b604d12d0b84bbe642e71f3cc10739418d3cdfd
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-08-20 16:55:25 -0300

    concertando a responssividade

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -8,20 +8,18 @@ const Resume = () => {
     <section className="container section resume" id="resume">
       <h2 className="section_title">Formação Acadêmica</h2>
       <div className="resume_container grid">
-        <div className="timeline grid">
-          {Data.map((val, id) => (
-            <Card
-              key={id}
-              icon={val.icon}
-              title={val.title}
-              year={val.year}
-              desc={val.desc}
-            />
-          ))}
-        </div>
+        {Data.map((val, id) => (
+          <Card
+            key={id}
+            icon={val.icon}
+            title={val.title}
+            year={val.year}
+            desc={val.desc}
+          />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Resume;
