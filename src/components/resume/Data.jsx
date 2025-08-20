ddd16f3b41f05b3c943b0e4812106f65bd106dcc
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-08-20 16:22:32 -0300

    concertando o resumo

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -1,41 +1,36 @@
 const Data = [
-   {
-        id: 1,
-        category: "educação",
-        icon: "icon-graduation",
-        year: '2022-2022',
-        title: "HTML5 e CSS3",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
- 
-    },
-    {
-        id: 3,
-        category: "experiência",
-        icon: "icon-graduation",
-        year: '2022-2023',
-        title: "JavaScript - Basico",
-        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
-    
-    },
-    {
-        id: 2,
-        category: "educação",
-        icon: "icon-graduation",
-        year: '2023-2024',
-        title: "JavaScript - Intermediário",
-        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
-        
-    },
-  
-    {
-        id: 4,
-        category: "experiência",
-        icon: "icon-graduation",
-        year: '2024-2024',
-        title: "JavaScript - Avançado",
-        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil placeat molestias voluptatem accusantium quidem hic, eaque impedit architecto reprehenderit repudiandae mollitia iure itaque officiis molestiae dicta perspiciatis ad nam commodi!",
-        
-    },
-]
+  {
+    id: 1,
+    category: "educação",
+    icon: "icon-graduation",
+    year: "2022-2022",
+    title: "HTML5 e CSS3",
+    desc: "Curso completo de HTML5 e CSS3, aprendendo a criar páginas responsivas e bem estruturadas.",
+  },
+  {
+    id: 2,
+    category: "educação",
+    icon: "icon-graduation",
+    year: "2023-2024",
+    title: "JavaScript - Intermediário",
+    desc: "Estudo aprofundado de funções, eventos, manipulação do DOM e consumo de APIs.",
+  },
+  {
+    id: 3,
+    category: "experiência",
+    icon: "icon-briefcase",
+    year: "2022-2023",
+    title: "JavaScript - Básico",
+    desc: "Primeiros projetos utilizando variáveis, loops, funções e estruturas condicionais.",
+  },
+  {
+    id: 4,
+    category: "experiência",
+    icon: "icon-briefcase",
+    year: "2024-2024",
+    title: "JavaScript - Avançado",
+    desc: "Desenvolvimento de aplicações com React, hooks, APIs e componentização avançada.",
+  },
+];
 
-export default Data
+export default Data;
