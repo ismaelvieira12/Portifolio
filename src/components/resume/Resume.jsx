19c8e8f7c42f7750504dd5909d2a63f1b8b23474
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-08-22 23:07:23 -0300

    Update

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,17 +4,31 @@ import Data from './Data';
 import Card from './Card';
 
 const Resume = () => {
+  const educacao = Data.filter( item => item.category === "educação");
+  const experiencia = Data.filter( item => item.category === "experiência");
   return (
     <section className="container section resume" id="resume">
       <h2 className="section_title">Formação Acadêmica</h2>
       <div className="resume_container grid">
-        {Data.map((val, id) => (
+        {educacao.map( item => (
           <Card
-            key={id}
-            icon={val.icon}
-            title={val.title}
-            year={val.year}
-            desc={val.desc}
+            key={item.id}
+            icon={item.icon}
+            title={item.title}
+            year={item.year}
+            desc={item.desc}
+          />
+        ))}
+      </div>
+
+      <div className="resume_container grid">
+        {experiencia.map( item => (
+          <Card
+            key={item.id}
+            icon={item.icon}
+            title={item.title}
+            year={item.year}
+            desc={item.desc}
           />
         ))}
       </div>
