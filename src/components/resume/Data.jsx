a484aa9b25dca9bcad36b969ebd9545bce9670c6
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-08-22 22:53:18 -0300

    update

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -1,36 +1,41 @@
 const Data = [
-  {
-    id: 1,
-    category: "educação",
-    icon: "icon-graduation",
-    year: '2022 - 2022',
-    title: "HTML5 e CSS3",
-    desc: "Curso completo de HTML5 e CSS3, aprendendo a criar páginas responsivas e bem estruturadas.",
-  },
-  {
-    id: 2,
-    category: "educação",
-    icon: "icon-graduation",
-    year: '2022 - 2023',
-    title: "JavaScript - Básico",
-    desc: "Primeiros projetos utilizando variáveis, loops, funções e estruturas condicionais.",
-  },
-  {
-    id: 3,
-    category: "educação",
-    icon: "icon-graduation",
-    year: '2023 - 2024',
-    title: "JavaScript - Intermediário",
-    desc: "Estudo aprofundado de funções, eventos, manipulação do DOM e consumo de APIs.",
-  },
-  {
-    id: 4,
-    category: "educação",
-    icon: "icon-graduation",
-    year: '2024 - 2024',
-    title: "JavaScript - Avançado",
-    desc: "Desenvolvimento de aplicações com React, hooks, APIs e componentização avançada.",
-  },
-];
+   {
+        id: 1,
+        category: "educação",
+        icon: "icon-graduation",
+        year: '2022-2022',
+        title: "HTML5 e CSS3",
+        desc: "Curso completo de HTML5 e CSS3, aprendendo a criar páginas responsivas e bem estruturadas",
+ 
+    },
+    {
+        id: 3,
+        category: "experiência",
+        icon: "icon-graduation",
+        year: '2022-2023',
+        title: "JavaScript - Basico",
+        desc:"Primeiros projetos utilizando variáveis, loops, funções e estruturas condicionais.",
+    
+    },
+    {
+        id: 2,
+        category: "educação",
+        icon: "icon-graduation",
+        year: '2023-2024',
+        title: "JavaScript - Intermediário",
+        desc:"Estudo aprofundado de funções, eventos, manipulação do DOM e consumo de APIs.",
+        
+    },
+  
+    {
+        id: 4,
+        category: "experiência",
+        icon: "icon-graduation",
+        year: '2024-2024',
+        title: "JavaScript - Avançado",
+        desc:"Desenvolvimento de aplicações com React, hooks, APIs e componentização avançada.",
+        
+    },
+]
 
-export default Data;
+export default Data
